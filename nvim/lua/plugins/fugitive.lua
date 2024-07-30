d00291acdc3d15a7947b47769fa9d49637ddca3b
bighi <contato@leonardobighi.com>
2024-07-30 14:08:52 -0300

    Add git Fugitive plugin

---
+++ nvim/lua/plugins/fugitive.lua
@@ -0,0 +1,12 @@
+return {
+	"tpope/vim-fugitive",
+	keys = {
+		{ "<leader>gg", "<cmd>Git<CR>", desc = "Open Git Fugitive" },
+		{ "<leader>gc", "<cmd>Git commit<CR>", desc = "Open Git Fugitive" },
+		{ "<leader>gpl", "<cmd>Git pull<CR>", desc = "Open Git Fugitive" },
+		{ "<leader>gps", "<cmd>Git push<CR>", desc = "Open Git Fugitive" },
+		{ "<leader>gb", "<cmd>Git blame<CR>", desc = "Open Git Fugitive" },
+		{ "<leader>gd", "<cmd>Git difftool<CR>", desc = "Open Git Fugitive" },
+		{ "<leader>gm", "<cmd>Git mergetool<CR>", desc = "Open Git Fugitive" },
+	},
+}
